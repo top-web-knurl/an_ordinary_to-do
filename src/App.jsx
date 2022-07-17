f6d40d1905cf24237e4b05056af7a8e192a8f188
Tim <topwebknurl@gmail.com>
2022-07-17 12:11:54 +0300

    Initialization of the project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/UI/Header/Header";
+
+
+function App() {
+  return (
+    <>
+      <Header>
+        <div className="App">
+        </div>
+      </Header>
+    </>
+
+  );
+}
+
+export default App;
